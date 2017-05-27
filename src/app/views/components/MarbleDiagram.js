54bfbc161d072bcc3c08a2f3ba011ecd80570fa8
jocelynhsu <rulinshyu@gmail.com>
2017-05-28 02:44:23 +0800

    add svg for marble diagram

--- src/app/views/components/MarbleDiagram.js
+++ src/app/views/components/MarbleDiagram.js
@@ -1,16 +1,52 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import classNames from 'classnames/bind';
+import styles from './MarbleDiagram.css';
+const cx = classNames.bind(styles);
 
 class MarbleDiagram extends Component {
     render() {
-        const { name, timestamp } = this.props.source;
+        const shiftY = 25;
 
         return (
-            <svg>
-                <text>{name}</text>
-                <text>{timestamp}</text>
-            </svg>
+            <div className={cx('container')}>
+                <svg className={cx('marble-diagram')}>
+                    <g transform={`translate(0, ${shiftY})`}>
+                        <line
+                            x1="0"
+                            x2="100%"
+                            y1="0"
+                            y2="0"
+                            stroke="#333"
+                            strokeWidth="2"
+                        />
+                        {this.props.sourceItems
+                            .filter(({ type }) => type === 'value')
+                            .map((item, index) => this.bubble(item, index))}
+                    </g>
+                </svg>
+            </div>
+        );
+    }
+
+    bubble(item, index) {
+        const { createAt } = this.props.source;
+        const shiftX = (item.timestamp - createAt) / 10;
+
+        return (
+            <g
+                className={cx('circle')}
+                key={index}
+                transform={`translate(${shiftX}, 0)`}
+            >
+                <circle cx="0" cy="0" r="20" stroke="#fff" strokeWidth="2" />
+                <text x="0" y="0" fill="#fff">{item.value}</text>
+            </g>
         );
     }
 }
 
-export default MarbleDiagram;
+export default connect((state, props) => ({
+    sourceItems: state.sourceItems[props.source.name],
+}))(MarbleDiagram);
