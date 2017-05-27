377bc5bf05c6b3f1b559f2a6abf3e2842d0f3d10
Jerry-Hong <jerry.hong@appier.com>
2017-05-27 11:05:47 +0800

    refactor content script

--- src/content-script/index.js
+++ src/content-script/index.js
@@ -1,53 +1,28 @@
-/*
-Inject Debuger Operator
+import { 
+    CONTENT_SCRIPT_CONNECT, 
+    CONTENT_SCRIPT_MESSAGE 
+} from '../constants/index.js';
+
+/** 
+ * Inject Debuger Operator
 */
-const script = require('raw-loader!./debugOperator.js');
+import script from 'raw-loader!../../build/tmp/debugOperator.js';
+
 const element = document.createElement('script');
 element.appendChild(document.createTextNode(script));
 (document.head || document.documentElement).appendChild(element);
 element.parentNode.removeChild(element);
-/*
-Connect Background 
+
+/**
+ * Connect Background 
 */
 const backgroundPageConnection = chrome.runtime.connect({
-    name: "content_script_conn"
+    name: CONTENT_SCRIPT_CONNECT
 });
 
-let debugLogs = [];
-let panelDataQueue = [];
-
-const tempData = (event) => {
-    debugLogs.push(event.detail);
-    panelDataQueue.push(event.detail);
-    passTempDataToDevtoolPanel();
-};
-
-const passTempDataToDevtoolPanel = () => {
-    while (panelDataQueue.length > 0) {
-        let data = panelDataQueue.shift();
-        backgroundPageConnection.postMessage({
-            name: 'content_script_msg',
-            params: data
-        });
-    }
-};
-
-backgroundPageConnection.onMessage.addListener((msg) => {
-    switch (msg.action) {
-        case 'devtools_panel_open':
-            panelDataQueue = [...debugLogs];
-            passTempDataToDevtoolPanel()
-            break;
-    
-        case 'devtools_panel_close':
-            window.document.removeEventListener('tempData', tempData, false);
-            break;
-    }
-});
-
-window.document.addEventListener('tempData', tempData, false);
-
-// trigger example:
-// document.dispatchEvent(new CustomEvent('tempData', { detail: { test: '123' } }));
-
-
+document.addEventListener('tempData', (event) => {
+    backgroundPageConnection.postMessage({
+        name: CONTENT_SCRIPT_MESSAGE,
+        params: event.detail
+    });
+}, false);
