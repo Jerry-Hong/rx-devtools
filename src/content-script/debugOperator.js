2bbd8d5e85fdd9bd5db769edc18c1545293c89ee
Jerry-Hong <s6323859@hotmail.com>
2017-05-27 22:12:57 +0800

    use constant

--- src/content-script/debugOperator.js
+++ src/content-script/debugOperator.js
@@ -1,8 +1,18 @@
 import { Subscription } from 'rxjs/Subscription';
 import { Subscriber } from 'rxjs/Subscriber';
+import {
+    RX_DEVTOOL_START,
+    RX_DEVTOOL_CUSTOM_EVENT,
+    OBSERVABLE_COMPLETE,
+    OBSERVABLE_CREATED,
+    OBSERVABLE_ERROR,
+    OBSERVABLE_NEXT,
+    OBSERVABLE_SUBSCRIBE,
+    OBSERVABLE_UNSUBSCRIBE
+} from '../constants/index.js';
 
 function createEvent(detail) {
-    return new CustomEvent('tempData', { detail });
+    return new CustomEvent(RX_DEVTOOL_CUSTOM_EVENT, { detail });
 }
 
 const sendEvent = data => document.dispatchEvent(createEvent(data));
@@ -13,38 +23,38 @@ class DebugSubscriber extends Subscriber {
         this.name = name;
         this.destination = destination;
         this.add(
-            new Subscription(() => {
-                sendEvent({ 
-                    source: name, 
-                    type: 'finally',
-                    timestamp: performance.now(),
-                });
-            })
-        );
+      new Subscription(() => {
+          sendEvent({
+              source: name,
+              type: OBSERVABLE_UNSUBSCRIBE,
+              timestamp: performance.now()
+          });
+      })
+    );
     }
     _next(value) {
         sendEvent({
             source: this.name,
-            type: 'next',
-            data: value,
-            timestamp: performance.now(),
+            type: OBSERVABLE_NEXT,
+            value: value,
+            timestamp: performance.now()
         });
         this.destination.next(value);
     }
     _error(error) {
         sendEvent({
             source: this.name,
-            type: 'error',
+            type: OBSERVABLE_ERROR,
             error: error,
-            timestamp: performance.now(),
+            timestamp: performance.now()
         });
         this.destination.error(error);
     }
     _complete() {
         sendEvent({
             source: this.name,
-            type: 'complete',
-            timestamp: performance.now(),
+            type: OBSERVABLE_COMPLETE,
+            timestamp: performance.now()
         });
         this.destination.complete();
     }
@@ -56,9 +66,9 @@ class DebugOperator {
     }
 
     call(subscriber, source) {
-        sendEvent({ 
-            source: this.name, 
-            type: 'subscribe',
+        sendEvent({
+            source: this.name,
+            type: OBSERVABLE_SUBSCRIBE,
             timestamp: performance.now()
         });
         return source.subscribe(new DebugSubscriber(subscriber, this.name));
@@ -66,10 +76,15 @@ class DebugOperator {
 }
 
 window.rxDevtool = function() {
+    sendEvent({
+        type: RX_DEVTOOL_START,
+        timestamp: performance.now()
+    });
+
     function debug(name) {
-        sendEvent({ 
-            source: name, 
-            type: 'create',
+        sendEvent({
+            source: name,
+            type: OBSERVABLE_CREATED,
             timestamp: performance.now()
         });
         return this.lift(new DebugOperator(name));
