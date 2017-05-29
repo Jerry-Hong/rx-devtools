64916831bcb28fa2e6b207d5a2bbfc5567a7ed04
Jerry-Hong <s6323859@hotmail.com>
2017-05-30 06:41:14 +0800

    modify routing

--- src/app/routers/index.js
+++ src/app/routers/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, IndexRedirect } from 'react-router';
 import App from '../views/layout/App.js';
 import SourceList from '../views/layout/SourceList.js';
 
 export default (
     <Route path="/" component={App}>
+        <IndexRedirect to="list" />
         <Route path="list" component={SourceList} />
     </Route>
 );
