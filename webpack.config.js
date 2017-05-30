495492e9abcf6ccb2913ea19b6c830515a8fa12b
Jerry-Hong <s6323859@hotmail.com>
2017-05-30 13:06:13 +0800

    remove optimize for develop

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,6 @@ module.exports = {
         ],
     },
     plugins: [
-        new webpack.optimize.OccurrenceOrderPlugin(),
         new CopyWebpackPlugin([
             {
                 context: './src/assets',
