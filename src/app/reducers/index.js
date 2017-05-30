3c9bfcaab005e52bb6475129c03f6a81da4ef57a
Jerry-Hong <s6323859@hotmail.com>
2017-05-30 15:43:56 +0800

    add currentTarget reducer

--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -3,9 +3,11 @@ import { routerReducer } from 'react-router-redux';
 
 import sources from './sources.js';
 import sourceItems from './sourceItems.js';
+import currentTarget from './currentTarget.js';
 
 export default combineReducers({
     routing: routerReducer,
     sources,
     sourceItems,
+    currentTarget,
 });
