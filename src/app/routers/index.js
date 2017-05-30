24cca9e7097808dfac02b192cf68731d5d4c451a
Jerry-Hong <s6323859@hotmail.com>
2017-05-30 15:06:53 +0800

    add Relation page

--- src/app/routers/index.js
+++ src/app/routers/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Route, IndexRedirect } from 'react-router';
+
 import App from '../views/layout/App.js';
 import Marble from '../views/Marble/Marble.js';
+import Relation from '../views/relation/Relation.js';
 
 export default (
     <Route path="/" component={App}>
         <IndexRedirect to="marble" />
         <Route path="marble" component={Marble} />
+        <Route path="relation" component={Relation} />
     </Route>
 );
---
+++ src/app/views/relation/Relation.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Relation extends Component {
+    render() {
+        return (
+            <div>
+                Not finish feature ~ Working on it
+            </div>
+        );
+    }
+}
+
+export default Relation;
